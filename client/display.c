b5b6a65aada75d02b77b2f56b678d16a8489aaec
Polo <polo.diemunsch@gmail.com>
2024-11-09 23:13:47 +0100

    nice ui

--- client/display.c
+++ client/display.c
@@ -1,62 +1,39 @@
 #include <stdio.h>
 #include <string.h>
+
 #include "display.h"
+#include "console.h"
 
 void update_interface(struct winsize ws) {
     clear_screen();
 
-    if (ws.ws_col < MIN_FULLSIZE_WIDTH)
-        display_halfsize(ws);
-    else
-        display_fullsize(ws);
-}
-
-void display_fullsize(struct winsize ws) {
-    if (ws.ws_col < MIN_FULLSIZE_WIDTH || ws.ws_row < MIN_HEIGHT) {
+    if (ws.ws_col < MIN_WIDTH || ws.ws_row < MIN_HEIGHT) {
         display_window_too_small(ws);
         return;
     }
 
-    unsigned short height = min(max(ws.ws_row, MIN_HEIGHT), MAX_HEIGHT);
-
-    unsigned short chat_width = ws.ws_col - GAME_WIDTH - 3 * HORIZONTAL_MARGIN;
-
-    Position game_position = {
-        .x = 1 + chat_width + 2 * HORIZONTAL_MARGIN,
-        .y = 1 + (height - GAME_HEIGHT) / 2,
-        .width = GAME_WIDTH,
-        .height = GAME_HEIGHT,
+    Position title_position = {
+        .x = 1,
+        .y = 1,
+        .width = ws.ws_col,
+        .height = TITLE_HEIGHT,
     };
-    display_game(game_position);
+    display_title(title_position);
+
+    Position menu_game_position = {
+        .x = 1,
+        .y = 1 + TITLE_HEIGHT,
+        .width = ws.ws_col,
+        .height = MENU_AND_GAME_HEIGHT,
+    };
+    // display_menu(menu_game_position);
+    display_game(menu_game_position);
 
     Position chat_position = {
-        .x = 1 + HORIZONTAL_MARGIN,
-        .y = 1 + VERTICAL_MARGIN,
-        .width = chat_width,
-        .height = height - 2 * VERTICAL_MARGIN,
-    };
-    display_chat(chat_position);
-}
-
-void display_halfsize(struct winsize ws) {
-    if (ws.ws_col < MIN_HALFSIZE_WIDTH || ws.ws_row < MIN_HALFSIZE_HEIGHT) {
-        display_window_too_small(ws);
-        return;
-    }
-
-    Position game_position = {
-        .x = (ws.ws_col - GAME_WIDTH) / 2,
-        .y = 1 + VERTICAL_MARGIN,
-        .width = GAME_WIDTH,
-        .height = GAME_HEIGHT,
-    };
-    display_game(game_position);
-
-    Position chat_position = {
-        .x = 1 + HORIZONTAL_MARGIN,
-        .y = 1 + GAME_HEIGHT + 2 * VERTICAL_MARGIN,
-        .width = ws.ws_col - 2 * HORIZONTAL_MARGIN,
-        .height = ws.ws_row - GAME_HEIGHT - 3 * VERTICAL_MARGIN,
+        .x = 1,
+        .y = 1 + TITLE_HEIGHT + MENU_AND_GAME_HEIGHT - VERTICAL_PADDING,
+        .width = ws.ws_col,
+        .height = ws.ws_row - TITLE_HEIGHT - MENU_AND_GAME_HEIGHT - VERTICAL_PADDING,
     };
     display_chat(chat_position);
 }
@@ -66,62 +43,161 @@ void display_window_too_small(struct winsize ws) {
     printf("Window too small, please resize it\n");
 }
 
-void display_chat(Position position) {
-    move_cursor_to(position.x, position.y);
-    printf("╭");
-    for (int i = 0; i < position.width - 2; i++) printf("─");
-    printf("╮");
+void display_title(Position position) {
+    move_cursor_to(1 + (position.width - 5) / 2, position.y + VERTICAL_PADDING);
+    color_printf("AWALÉ\n", BCYAN);
+}
 
-    for (int i = 1; i < position.height - 1; i++) {
-        move_cursor_to(position.x, position.y + i);
-        printf("│");
-        move_cursor_to(position.x + position.width - 1, position.y + i);
-        printf("│");
-    }
+void display_menu(Position position) {
+    unsigned short first_column = (position.width - MENU_AND_GAME_WIDTH) / 2 + HORIZONTAL_PADDING;
 
-    move_cursor_to(position.x, position.y + position.height - 1);
-    printf("╰");
-    for (int i = 0; i < position.width - 2; i++) printf("─");
-    printf("╯");
+    move_cursor_to(1 + (position.width - 7) / 2, position.y + VERTICAL_PADDING);
+    color_printf("GENERAL\n", BWHITE);
 
-    move_cursor_to(position.x + 1, position.y + position.height - 2);
-    printf("> ");
+    move_cursor_right(first_column);
+    color_printf("send ", BYELLOW);
+    printf("[");
+    color_printf("player", BBLUE);
+    printf("|");
+    color_printf("all", BRED);
+    printf("] message    send a message to a player / to everyone\n");
 
-    fflush(stdout);
+    move_cursor_right(first_column);
+    color_printf("challenge ", BYELLOW);
+    color_printf("player", BBLUE);
+    printf("             challenge a player to a duel / accept\n");
+    move_cursor_right(first_column);
+    printf("                             the challenge from someone\n");
+
+    move_cursor_right(first_column);
+    color_printf("cancel_challenge ", BYELLOW);
+    color_printf("player", BBLUE);
+    printf("      cancel a challenge sent to a player\n");
+
+    move_cursor_right(first_column);
+    color_printf("historic ", BYELLOW);
+    printf("(");
+    color_printf("n", BCYAN);
+    printf(")");
+    printf("                 list your (n) past games\n");
+    move_cursor_right(first_column);
+    color_printf("replay ", BYELLOW);
+    color_printf("game", BCYAN);
+    printf("                  watch the replay of a game\n");
+
+    printf("\n");
+    move_cursor_right((position.width - 7) / 2);
+    color_printf("IN GAME\n", BWHITE);
+
+    move_cursor_right(first_column);
+    color_printf("play ", BYELLOW);
+    color_printf("slot", BGREEN);
+    printf("                    choose which slot to play\n");
+
+    printf("\n");
+    move_cursor_right((position.width - 6) / 2);
+    color_printf("REPLAY\n", BWHITE);
+
+    move_cursor_right(first_column);
+    color_printf("next ", BYELLOW);
+    printf("                        display next move\n");
 }
 
 void display_game(Position position) {
     int board[BOARD_SIZE] = {1, 2, 4, 6, 8, 12, 3, 0, 6, 8, 2, 3};
+    char name_player1[] = "Mais what the fuck il est vraiment super archi mega long ton pseudo";
+    unsigned int score_player1 = 69;
     char name_player2[] = "Joueur adverse";
     unsigned int score_player2 = 0;
-    char name_player1[] = "Mais wtf il est super long ton pseudo";
-    unsigned int score_player1 = 69;
-    char displayed_name[DISPLAYED_NAME_MAX_SIZE + 1];
+    unsigned int player = 0;
+    unsigned int turn = 22;
 
-    move_cursor_to(position.x, position.y);
-    printf(" %-26s%2d", truncate_name(displayed_name, name_player2, DISPLAYED_NAME_MAX_SIZE), score_player2);
+    int start_index, offset;
 
-    move_cursor_to(position.x, position.y + 2);
-    printf("╭────┬────┬────┬────┬────┬────╮");
+    char display_string[MENU_AND_GAME_WIDTH + 1];
 
-    move_cursor_to(position.x, position.y + 3);
-    for (int i = BOARD_SIZE - 1; i >= BOARD_SIZE / 2; i--)
+    unsigned short is_my_turn = turn % 2 != player;
+
+    sprintf(display_string, "Turn %d - ", turn);
+    char who_is_playing[MENU_AND_GAME_WIDTH + 1];
+    is_my_turn ? strcpy(who_is_playing, "Your time to play") : strcpy(who_is_playing, "Waiting for opponent to play");
+
+    move_cursor_to((int) (1 + (position.width - strlen(display_string) - strlen(who_is_playing)) / 2), position.y + VERTICAL_PADDING);
+    printf(display_string);
+    color_printf(who_is_playing, is_my_turn ? BBLUE : MAGENTA);
+    printf("\n\n\n");
+
+    char opponent[] = " (Opponent)";
+    offset = strlen(opponent);
+    truncate_name(display_string, name_player2, MENU_AND_GAME_WIDTH - 2 * HORIZONTAL_PADDING - offset);
+    move_cursor_right((int)(position.width - strlen(display_string) - offset) / 2);
+    color_printf(display_string, MAGENTA);
+    printf("%s\n\n", opponent);
+
+    unsigned short board_left_column = (position.width - BOARD_WIDTH) / 2;
+
+    move_cursor_right(board_left_column);
+    printf("╭────┬────┬────┬────┬────┬────╮\n");
+
+    sprintf(display_string, "%2d   ", score_player2);
+    move_cursor_right((int)(board_left_column - strlen(display_string)));
+    color_printf(display_string, MAGENTA);
+    start_index = (player == 0 ? BOARD_SIZE : BOARD_SIZE / 2) - 1;
+    for (int i = start_index; i >= start_index - BOARD_SIZE / 2 + 1; i--)
         printf("│ %2d ", board[i]);
     printf("│");
+    color_printf("  ◀─╮\n", BBLACK);
 
-    move_cursor_to(position.x, position.y + 4);
+    move_cursor_right(board_left_column);
     printf("├────┼────┼────┼────┼────┼────┤");
+    color_printf("    │\n", BBLACK);
 
-    move_cursor_to(position.x, position.y + 5);
-    for (int i = 0; i < BOARD_SIZE / 2; i++)
+    sprintf(display_string, "%2d   ", score_player1);
+    move_cursor_right((int)(board_left_column - strlen(display_string)));
+    color_printf(display_string, BBLUE);
+    start_index = (player == 0 ? 0 : BOARD_SIZE / 2);
+    for (int i = start_index; i < start_index + BOARD_SIZE / 2; i++)
         printf("│ %2d ", board[i]);
     printf("│");
+    color_printf("   ─╯\n", BBLACK);
 
-    move_cursor_to(position.x, position.y + 6);
-    printf("╰────┴────┴────┴────┴────┴────╯");
+    move_cursor_right(board_left_column);
+    printf("╰────┴────┴────┴────┴────┴────╯\n");
 
-    move_cursor_to(position.x, position.y + 8);
-    printf(" %-26s%2d", truncate_name(displayed_name, name_player1, DISPLAYED_NAME_MAX_SIZE), score_player1);
+    move_cursor_right(board_left_column);
+    color_printf("  a    b    c    d    e    f\n\n", BBLACK);
+
+    char you[] = " (You)";
+    offset = strlen(you);
+    truncate_name(display_string, name_player1, MENU_AND_GAME_WIDTH - 2 * HORIZONTAL_PADDING - offset);
+    move_cursor_right((int)(position.width - strlen(display_string) - offset) / 2);
+    color_printf(display_string, BBLUE);
+    printf("%s\n\n", you);
+}
+
+void display_chat(Position position) {
+    unsigned short first_column = (position.width - CHAT_WIDTH) / 2 + HORIZONTAL_PADDING;
+    unsigned short content_length = CHAT_WIDTH - 2 * HORIZONTAL_PADDING - 2;
+
+    move_cursor_to(1 + first_column, position.y + VERTICAL_PADDING);
+    printf("╭");
+    for (int i = 0; i < content_length; i++) printf("─");
+    printf("╮\n");
+
+    for (int i = 1; i < position.height - 1; i++) {
+        move_cursor_right(first_column);
+        printf("│");
+        move_cursor_right(content_length);
+        printf("│\n");
+    }
+
+    move_cursor_right(first_column);
+    printf("╰");
+    for (int i = 0; i < content_length; i++) printf("─");
+    printf("╯\n");
+
+    move_cursor_to(1 + first_column + 1, position.y + VERTICAL_PADDING + 1);
+    printf("> ");
 
     fflush(stdout);
 }
@@ -130,8 +206,7 @@ char * truncate_name(char * destination, char * source, size_t n) {
     strncpy(destination, source, n);
 
     if (strlen(source) > n) {
-        strcpy(destination + n - 4, "...");
-        destination[n] = '\0';
+        strcpy(destination + n - 3, "...");
     }
 
     return destination;
--- client/display.h
+++ client/display.h
@@ -4,27 +4,22 @@
 #include <sys/ioctl.h>
 
 // TODO define somewhere else ?
-#define BOARD_SIZE 12
+#define BOARD_SIZE           12
 
-#define VERTICAL_MARGIN 1
-#define HORIZONTAL_MARGIN (2 * VERTICAL_MARGIN)
-#define MAX_CHAT_WIDTH 100
-#define GAME_WIDTH ((BOARD_SIZE / 2) * 5 + 1)
-#define GAME_HEIGHT 9
-#define MIN_HEIGHT (GAME_HEIGHT + 2 * HORIZONTAL_MARGIN)
-#define MAX_HEIGHT 25
-#define MIN_FULLSIZE_WIDTH (2 * GAME_WIDTH + 3 * HORIZONTAL_MARGIN)
-#define MIN_HALFSIZE_WIDTH (GAME_WIDTH + 2 * HORIZONTAL_MARGIN)
-#define MIN_HALFSIZE_HEIGHT (2 * GAME_HEIGHT + 3 * VERTICAL_MARGIN)
+#define VERTICAL_PADDING        1
+#define HORIZONTAL_PADDING      (2 * VERTICAL_PADDING)
 
-#define DISPLAYED_NAME_MAX_SIZE 26
+#define TITLE_HEIGHT            (1 + VERTICAL_PADDING)
 
-#define clear_screen() printf("\033[1;1H\033[2J")
-#define move_cursor_to(x,y) printf("\033[%d;%dH", (y), (x))
-#define move_cursor_up(x) printf("\033[%dA", (x))
-#define move_cursor_down(x) printf("\033[%dB", (x))
-#define move_cursor_right(x) printf("\033[%dC", (x))
-#define move_cursor_left(x) printf("\033[%dD", (x))
+#define MENU_AND_GAME_WIDTH     (69 + 2 * HORIZONTAL_PADDING)
+#define MENU_AND_GAME_HEIGHT    (13 + 2 * VERTICAL_PADDING)
+
+#define BOARD_WIDTH             ((BOARD_SIZE / 2) * 5 + 1)
+
+#define CHAT_WIDTH              (MENU_AND_GAME_WIDTH + 2)
+
+#define MIN_WIDTH               CHAT_WIDTH
+#define MIN_HEIGHT              (TITLE_HEIGHT + 2 * MENU_AND_GAME_HEIGHT - VERTICAL_PADDING)
 
 #define max(a,b) ((a) > (b) ? (a) : (b))
 #define min(a,b) ((a) < (b) ? (a) : (b))
@@ -41,8 +36,10 @@ typedef struct {
     unsigned short height;
 } Position;
 
-void display_chat(Position position);
+void display_title(Position position);
+void display_menu(Position position);
 void display_game(Position position);
+void display_chat(Position position);
 
 char * truncate_name(char * destination, char * source, size_t n);
 
