eaa17222f3dcb53d029a8883ac82ef6e0fc0eb78
Polo <polo.diemunsch@gmail.com>
2024-11-15 09:29:52 +0100

    chars in awale

--- Game/awale.c
+++ Game/awale.c
@@ -10,35 +10,42 @@
 #define TRUE 1
 
 //useful functions
-int slot_belongs_to_player(int player, int slot) {
-    //returns TRUE if slot belongs to player, FALSE if it doesn't and -1 if out or range
-    if (slot < 0 || slot >= 12) {
+int slot_belongs_to_player(char player, char slot)
+{
+    //returns TRUE if slot belongs to player, FALSE if it doesn't and -1 if out of range
+    if (slot < 0 || slot >= 12)
+    {
         printf("Slot is out of range!\n");
         return -1;
     }
     return (player == 0 && slot < 6) || (player == 1 && slot >= 6);
 }
 
-int player_is_broke (Game * game, int player) {
-    //returns 0 if player is broke, 1 otherwise
-    for (int i=0;i<12;++i){
-        if(slot_belongs_to_player(player,i)) {
-            if(game->board[i]!=0){
+int player_is_broke(Game *game, char player)
+{
+    //returns 1 if player is broke, 0 otherwise
+    for (int i=0;i<12;++i)
+    {
+        if (slot_belongs_to_player(player,i))
+        {
+            if (game->board[i]!=0)
                 return 0;
-            }
         }
     }
     return 1;
 }
 
-int distribute_seeds(Game*game, int slot){
+char distribute_seeds(Game *game, char slot)
+{
    int seeds=game->board[slot];
     game->board[slot]=0; //empty slot where seeds were taken
 
-    int current_slot=slot;
-    for (int i=1;i<=seeds;++i) {
+    char current_slot=slot;
+    for (int i=1;i<=seeds;++i)
+    {
         current_slot = (current_slot+1)%12; //slot in which we are about to add a seed
-        if(current_slot==slot){ //except if it is the one we took the seeds in
+        if(current_slot==slot) //except if it is the one we took the seeds in
+        {
             i--;
             continue;
         }
@@ -47,21 +54,26 @@ int distribute_seeds(Game*game, int slot){
     return current_slot;
 }
 
-int collect_seeds(Game * game, int player, int current_slot){
-    while ((game->board[current_slot]==2 || game->board[current_slot]==3) && !slot_belongs_to_player(player,current_slot)){ 
+void collect_seeds(Game *game, char player, char current_slot)
+{
+    while ((game->board[current_slot]==2 || game->board[current_slot]==3) && !slot_belongs_to_player(player,current_slot))
+    { 
         game->players[player].score+=game->board[current_slot]; //increment score
         game->board[current_slot]=0; //empty slot
         current_slot --;
     }
 }
 
-int is_feeding_move(Game*game,int player, int slot){
-    Game * game_copy = malloc(sizeof(Game));
+char is_feeding_move(Game *game, char player, char slot)
+{
+    Game *game_copy = malloc(sizeof(Game));
     *game_copy=*game;
-    int other_player=(player+1)%2;
-    if(slot_belongs_to_player(player,slot)){
+    char other_player=(player+1)%2;
+    if(slot_belongs_to_player(player,slot))
+    {
         distribute_seeds(game_copy,slot);
-        if(!player_is_broke(game_copy,other_player)){
+        if(!player_is_broke(game_copy,other_player))
+        {
             free(game_copy);
             return 1;
         }
@@ -70,30 +82,33 @@ int is_feeding_move(Game*game,int player, int slot){
     return 0;
 }
 
-int is_starving_move (Game * game, int player, int current_slot){
+char is_starving_move(Game *game, char player, char current_slot)
+{
     int other_player=(player+1)%2;
-    Game * game_copy = malloc(sizeof(Game));
+    Game *game_copy = malloc(sizeof(Game));
     *game_copy=*game;
     collect_seeds(game_copy,player,current_slot);
-    if (player_is_broke(game_copy,other_player)){
+    if (player_is_broke(game_copy,other_player))
         return 1;
-    }
+
     return 0;
 }
 
-int exists_move_to_feed (Game * game, int player) { 
+char exists_move_to_feed(Game *game, char player)
+{ 
     //returns 1 if there exists a move to feed the other player
-    int other_player=(player+1)%2;
-    for (int i=0;i<12;++i){
-        if(is_feeding_move(game,player,i)){
+    char other_player=(player+1)%2;
+    for (int i=0;i<12;++i)
+    {
+        if(is_feeding_move(game,player,i))
             return 1;
-        }
     }
     return 0;
 }
 
 
-Game * create_game(char * player0_name, char * player1_name, int direction) {
+Game * create_game(char *player0_name, char *player1_name, char direction)
+{
     srand(time(NULL));
     Player player0 = {
         .name = player0_name,
@@ -106,11 +121,12 @@ Game * create_game(char * player0_name, char * player1_name, int direction) {
     };
 
     Game *game = malloc(sizeof(Game));
-    if (game == NULL) {
+    if (game == NULL)
+    {
         perror("Failed to allocate memory for game");
         return NULL;
     }
-    *game = (Game){
+    *game = (Game) {
         .players = {player0, player1},
         .board = {4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4},
         .round = 0,
@@ -121,35 +137,44 @@ Game * create_game(char * player0_name, char * player1_name, int direction) {
 }
 
 //main function
-int execute_round(Game * game, int player, int slot) {
+int execute_round(Game *game, char player, char slot)
+{
     //returns 0 if goes correctly
     //returns -1 if the move was not valid
     //returns 1 if game over
-    if (game->turn != player) {
+    if (game->turn != player)
+    {
         printf("Wrong player!\n");
         return -1;
     }
     
-    if (slot_belongs_to_player(player, slot) != 1) {
+    if (slot_belongs_to_player(player, slot) != 1)
+    {
         printf("Slot does not belong to player!\n");
         return -1;
     }
 
-    if (game->board[slot]==0){
+    if (game->board[slot]==0)
+    {
         printf("Slot is empty!");
         return -1;
     }
 
     //check if the move feeds the other player if necessary
-    int other_player = ((player+1)%2);
-    if(player_is_broke(game,other_player)) {
+    char other_player = ((player+1)%2);
+    if(player_is_broke(game,other_player))
+    {
         printf("Other player is broke, you have to feed him\n");
-        if(!is_feeding_move(game,player,slot) && exists_move_to_feed(game,player)){
+        if(!is_feeding_move(game,player,slot) && exists_move_to_feed(game,player))
+        {
             printf("You have to select a move that feeds the other player!");
             return -1;
-        } else {
+        }
+        else
+        {
             //increment score (all seeds go to the player)
-            for (int i=0;i<12;++i){
+            for (int i=0;i<12;++i)
+            {
                 game->players[player].score+=game->board[i];
                 game->board[i]=0;
                 return 1;
@@ -158,17 +183,18 @@ int execute_round(Game * game, int player, int slot) {
         }
     }
 
-    int current_slot=distribute_seeds(game,slot);
+    char current_slot=distribute_seeds(game,slot);
 
     //check if seeds are gained
-    if ((game->board[current_slot]==2 || game->board[current_slot]==3) && !slot_belongs_to_player(player,current_slot)){
-        if(!is_starving_move(game,player,slot)){
+    if ((game->board[current_slot]==2 || game->board[current_slot]==3) && !slot_belongs_to_player(player,current_slot))
+    {
+        if(!is_starving_move(game,player,slot))
             collect_seeds(game,player,current_slot);
-        }
     }
 
     //check if a player won with their score
-    if(game->players[player].score>24) {
+    if(game->players[player].score>24)
+    {
         printf("%s is the winner!",game->players[player].name);
         return 1;
     }
@@ -181,20 +207,19 @@ int execute_round(Game * game, int player, int slot) {
     return 0;
 }
 
-
-
-void print_game (Game * game) { 
+void print_game(Game *game)
+{ 
     printf("Players : %s and %s \n",game->players[0].name, game->players[1].name);
     printf("Round : %d", game->round);
     printf("     Turn : %s \n",game->players[game->turn].name);
     printf("Board : \n");
-    for(int i=11;i>5;--i){
+    for(int i=11;i>5;--i)
         printf("%d ",game->board[i]);
-    }
+
     printf("\n");
-    for(int i=0;i<6;++i){
+    for(int i=0;i<6;++i)
         printf("%d ",game->board[i]);
-    }
+
     printf("\n");
     printf("Scores :     %s: %d      %s: %d\n",game->players[0].name,game->players[0].score,game->players[1].name,game->players[1].score);
     printf("\n\n");
--- Game/awale.h
+++ Game/awale.h
@@ -4,21 +4,27 @@
 typedef struct
 {
     char * name;
-    unsigned int score;
+    char score;
 } Player;
 
 typedef struct
 {
     Player players[2];
-    unsigned int board[12];
-    unsigned int round;
-    unsigned int turn;
-    unsigned int direction; //clockwise=0, anti-clockwise=1
+    char board[12];
+    char round;
+    char turn;
+    char direction; //clockwise=0, anti-clockwise=1
 } Game;
 
-int slot_belongs_to_player (int player, int slot);
-Game * create_game(char * player1_name, char * player2_name, int direction);
-int execute_round(Game * game, int player, int slot);
+char slot_belongs_to_player(char player, char slot);
+int player_is_broke(Game *game, char player);
+char distribute_seeds(Game *game, char slot);
+void collect_seeds(Game *game, char player, char current_slot);
+char is_feeding_move(Game *game, char player, char slot);
+char is_starving_move(Game *game, char player, char current_slot);
+char exists_move_to_feed(Game *game, char player);
+Game * create_game(char * player1_name, char * player2_name, char direction);
+int execute_round(Game * game, char player, char slot);
 void print_game (Game * game);
 
 #endif /* guard */
